9771d2c6745659b8b65fa1908a2427868b584aac
Kishan Godhani <kishangodhani@outlook.com>
2022-09-13 19:29:10 +0530

    Update SampleTest.py

--- SampleTest.py
+++ SampleTest.py
@@ -1,22 +1,14 @@
 from appium import webdriver
 
 desired_caps = {
-    # "platformName": "Android",
-    # # Xiaomi Redmi Note 10
-    # "deviceName": "b5af8c6b",
-    # "version": "11",
-
     "platformName": "Android",
-    # Samsung M20
-    # "deviceName": "320194a65f3f56e9",
+    "deviceName": "bhihcjdicubdwijwdbc",
     "version": "9",
 }
 
-# Add your selenium grid appium public URL here.
-driver = webdriver.Remote("http://15.206.213.81:4444", desired_caps)
+# Create driver and start launch the excution.
+driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
 
 # Run your app page source and quite after run.
 print(driver.page_source)
 driver.quit()
-
-
